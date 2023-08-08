0286dab777428ab9d8e41c499e210ab656367fb9
Rubcs <Rubcs@gmail.com>
2023-08-08 12:32:55 +0200

    create PlayerButton component

--- piedra_papel_tijera_lagarto_shpock/src/components/GameBoard/GameBoard.jsx
+++ piedra_papel_tijera_lagarto_shpock/src/components/GameBoard/GameBoard.jsx
@@ -1,8 +1,20 @@
+import PlayerButton from "../PlayerButton";
 import "./GameBoard.css";
 import React from "react";
 
 const GameBoard = () => {
-  return <>THIS IS THE GAME BOARD</>;
+  return (
+    <article className="game-board">
+      <section className="players-container">
+        <div className="player-one-container"></div>
+        <div className="player-two-container"></div>
+      </section>
+
+      <section className="player-button-container">
+        <PlayerButton />
+      </section>
+    </article>
+  );
 };
 
 export default GameBoard;
---
+++ piedra_papel_tijera_lagarto_shpock/src/components/PlayerButton.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PlayerButton = () => {
+  const playerButtonhandler = () => {};
+
+  return (
+    <button className="play-button" onClick={playerButtonhandler}>
+      CLICK
+    </button>
+  );
+};
+
+export default PlayerButton;
