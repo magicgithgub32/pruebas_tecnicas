7acb8789cd46d001faf92ab414e3d5f91b3a4313
Rubcs <Rubcs@gmail.com>
2023-08-10 11:19:23 +0200

    remove PPTLS

--- piedra_papel_tijera_lagarto_spock/src/components/Button/Button.jsx
+++
@@ -1,8 +0,0 @@
-import "./Button.css";
-import React from "react";
-
-const Button = ({ text }) => {
-  return <button className="play-button">{text}</button>;
-};
-
-export default Button;
--- piedra_papel_tijera_lagarto_spock/src/components/Footer/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import "./Footer.css";
-import React from "react";
-
-const Footer = () => {
-  return <footer className="footer">Powered by Rubén Piqueras - 2023</footer>;
-};
-
-export default Footer;
